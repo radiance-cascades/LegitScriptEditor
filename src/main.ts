56d1d6132d48c8f7e1396bfc4e366e4e78e3cf80
Elijah Insua <tmpvar@gmail.com>
2024-10-15 19:03:48 -0700

    add in the editor

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,62 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as monaco from 'monaco-editor'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
+self.MonacoEnvironment = {
+	getWorkerUrl: function (moduleId, label) {
+		if (label === 'json') {
+			return './json.worker.bundle.js';
+		}
+		if (label === 'css' || label === 'scss' || label === 'less') {
+			return './css.worker.bundle.js';
+		}
+		if (label === 'html' || label === 'handlebars' || label === 'razor') {
+			return './html.worker.bundle.js';
+		}
+		if (label === 'typescript' || label === 'javascript') {
+			return './ts.worker.bundle.js';
+		}
+		return './editor.worker.bundle.js';
+	}
+};
+
+
+const initialContent = `
+void ColorPass(in float r, in float g, in float b, out vec4 out_color)
+{{
+  void main()
+  {
+    out_color = vec4(r, g, b + 0.5f, 1.0f);
+  }
+}}
+void RenderGraphMain()
+{{
+  void main()
+  {
+    Image img = GetImage(ivec2(128, 128), rgba8);
+    ColorPass(
+      SliderFloat("R", 0.0f, 1.0f) + 0.5f,
+      SliderFloat("G", 0.0f, 1.0f),
+      SliderFloat("B", 0.0f, 1.0f),
+      GetSwapchainImage());
+    int a = SliderInt("Int param", -42, 42, 5);
+    float b = SliderFloat("Float param", -42.0f, 42.0f);
+    //float e = SliderFloat("Float param", -42.0f, 42.0f);
+    Text("script int: " + formatInt(a) + " float: " + formatFloat(b));
+  }
+}}
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const editorEl = document.getElementById('editor');
+if (editorEl) {
+  const editor = monaco.editor.create(editorEl, {
+    value: initialContent || '',
+    language: 'c',
+    minimap: {
+      enabled: false
+    },
+    tabSize: 2,
+    automaticLayout: true,
+    theme: 'vs-dark'
+  })
+  editor.focus()
+}
