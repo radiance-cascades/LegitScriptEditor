16da35c2780fa04feee22ca030b32e1a480b434d
Elijah Insua <tmpvar@gmail.com>
2024-10-15 19:13:35 -0700

    fix worker warning in console

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,11 @@
 import './style.css'
 import * as monaco from 'monaco-editor'
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
 
 self.MonacoEnvironment = {
-	getWorkerUrl: function (moduleId, label) {
-		if (label === 'json') {
-			return './json.worker.bundle.js';
-		}
-		if (label === 'css' || label === 'scss' || label === 'less') {
-			return './css.worker.bundle.js';
-		}
-		if (label === 'html' || label === 'handlebars' || label === 'razor') {
-			return './html.worker.bundle.js';
-		}
-		if (label === 'typescript' || label === 'javascript') {
-			return './ts.worker.bundle.js';
-		}
-		return './editor.worker.bundle.js';
-	}
+  getWorker: function (moduleId, label) {
+    return new editorWorker();
+  }
 };
 
 
