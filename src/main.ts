dec11ad437c87d4c6b67d37e74fccff7a4a4d1f8
Raikiri <donxenapo@gmail.com>
2024-10-21 01:36:16 +1300

    More refactoring

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,10 @@ import {
   LegitScriptImmediateModeControlCallbacks,
   LegitScriptLoadResult,
   RaisesErrorFN,
+  LegitScriptContextRequest,
+  LegitScriptShaderInvocation,
+  FramegraphPasses,
+  ivec2
 } from "./types"
 
 // @ts-ignore
@@ -222,11 +226,6 @@ function UpdateFramegraph(
         msg : res.msg,
         type: 'fail'
       }
-      /*raiseError(
-        `Compilation of ${desc.name} failed at line ${src_line} with error ${res.msg}`
-      )*/
-
-      continue
     }
     if(res.type === 'success')
     {
@@ -524,8 +523,7 @@ async function Init(
 
 //there's no way in gles 3.0 to attach the backbuffer as part of an fbo. so we have to crate a temporary texture instead of the back buffer
 //and at the end of the frame copy it onto the back buffer
-function CopyTexToSwapchain(state: State, tex : WebGLTexture | null){
-  const gpu = state.gpu
+function CopyTexToSwapchain(gpu: GPUState, tex : WebGLTexture | null){
   const gl = gpu.gl
   gl.bindFramebuffer(gl.FRAMEBUFFER, null);
   gl.viewport(0, 0, gpu.canvas.width, gpu.canvas.height)
@@ -534,6 +532,176 @@ function CopyTexToSwapchain(state: State, tex : WebGLTexture | null){
   gl.useProgram(gpu.copyProgram)
   gpu.fullScreenRenderer()
 }
+
+
+
+function ProcessScriptRequests(
+  imControlsCallbacks : LegitScriptImmediateModeControlCallbacks,
+  imageCache : ImageCache,
+  swapchainSize : ivec2,
+  gl : WebGL2RenderingContext,
+  contextRequests : LegitScriptContextRequest[]) : LegitScriptContextInput[]
+{
+  var contextInputs : LegitScriptContextInput[] = [];
+  ImageCacheStartFrame(
+    gl,
+    imageCache,
+  )
+  //This image will be copied into the swapchain
+  ImageCacheProcessRequest(
+    gl,
+    imageCache,
+    {id : 0, pixel_format : 'rgba8', size : swapchainSize, type : 'CachedImageRequest'},
+    console.error)
+
+  for(const request of contextRequests){
+    if(request.type === 'CachedImageRequest'){
+      ImageCacheProcessRequest(
+        gl,
+        imageCache,
+        request,
+        console.error
+      )
+    }
+    if(request.type == 'FloatRequest'){
+      contextInputs.push({
+        name : request.name,
+        type : 'float',
+        value : imControlsCallbacks.floatSlider(request.name, request.def_val, request.min_val, request.max_val)
+      });
+    }
+    if(request.type == 'IntRequest'){
+      contextInputs.push({
+        name : request.name,
+        type : 'int',
+        value : imControlsCallbacks.intSlider(request.name, request.def_val, request.min_val, request.max_val)
+      });
+    }
+    if(request.type == 'TextRequest'){
+      imControlsCallbacks.text(request.text)
+    }
+    if(request.type == 'BoolRequest'){
+      contextInputs.push({
+        name : request.name,
+        type : 'int',
+        value : 1 //TODO: actually make a checkbox
+      });
+    }
+    if(request.type == 'LoadedImageRequest'){
+      //TODO: figure this out
+    }
+  }
+  return contextInputs;
+}
+
+function RunScriptInvocations(
+  imageCache: ImageCache,
+  gpu: GPUState,
+  passes: FramegraphPasses,
+  shaderInvocations : LegitScriptShaderInvocation[])
+{
+  const gl = gpu.gl;
+  for (const invocation of shaderInvocations) {
+    const pass = passes[invocation.shader_name]
+    gl.useProgram(pass.program)
+
+    for (
+      let uniformIndex = 0;
+      uniformIndex < invocation.uniforms.length;
+      uniformIndex++
+    ) {
+      const uniform = invocation.uniforms[uniformIndex]
+      if (!uniform) {
+        continue
+      }
+
+      switch (uniform.type) {
+        case 'float': {
+          gl.uniform1f(pass.uniforms[uniformIndex], uniform.value)
+          break
+        }
+        case 'vec2': {
+          gl.uniform2f(pass.uniforms[uniformIndex], uniform.value.x, uniform.value.y)
+          break
+        }
+        case 'vec3': {
+          gl.uniform3f(pass.uniforms[uniformIndex], uniform.value.x, uniform.value.y, uniform.value.z)
+          break
+        }
+        case 'vec4': {
+          gl.uniform4f(pass.uniforms[uniformIndex], uniform.value.x, uniform.value.y, uniform.value.z, uniform.value.w)
+          break
+        }
+        case "int": {
+          gl.uniform1i(pass.uniforms[uniformIndex], uniform.value)
+          break
+        }
+        case 'ivec2': {
+          gl.uniform2i(pass.uniforms[uniformIndex], uniform.value.x, uniform.value.y)
+          break
+        }
+        case 'ivec3': {
+          gl.uniform3i(pass.uniforms[uniformIndex], uniform.value.x, uniform.value.y, uniform.value.z)
+          break
+        }
+        case 'ivec4': {
+          gl.uniform4i(pass.uniforms[uniformIndex], uniform.value.x, uniform.value.y, uniform.value.z, uniform.value.w)
+          break
+        }
+      }
+    }
+
+    for (
+      let samplerIndex = 0;
+      samplerIndex < invocation.image_sampler_bindings.length;
+      samplerIndex++
+    ) {
+      const sampler = invocation.image_sampler_bindings[samplerIndex]
+      const handle = ImageCacheGetImage(imageCache, sampler.id)
+      if (!handle) {
+        console.error("missing image from image cache %s", sampler)
+      }
+
+      gl.activeTexture(gl.TEXTURE0 + samplerIndex)
+      gl.bindTexture(gl.TEXTURE_2D, handle)
+      gl.uniform1i(pass.samplers[samplerIndex], samplerIndex)
+      gl.bindTexture(gl.TEXTURE_2D, handle)
+    }
+
+    // special case for swapchain image
+    {
+      // TODO: bind more than one output
+      gl.bindFramebuffer(gl.FRAMEBUFFER, pass.fbo)
+      
+      if (invocation.color_attachments.length != pass.fboAttachmentIds.length) {
+        console.error("Mismatch in invocation vs pass description attachment count")
+      }
+      for (
+        let attachmentIndex = 0;
+        attachmentIndex < invocation.color_attachments.length;
+        attachmentIndex++
+      ) {
+        const attachment = invocation.color_attachments[attachmentIndex]
+        const target = ImageCacheGetImage(imageCache, attachment.id)
+        gl.framebufferTexture2D(
+          gl.FRAMEBUFFER,
+          pass.fboAttachmentIds[attachmentIndex],
+          gl.TEXTURE_2D,
+          target,
+          0
+        )
+      }
+      // TODO: handle framebuffer completeness
+      gl.drawBuffers(pass.fboAttachmentIds)
+    }
+
+    gl.viewport(0, 0, gpu.canvas.width, gpu.canvas.height)
+    gpu.fullScreenRenderer()
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null)
+  }
+  CopyTexToSwapchain(gpu, ImageCacheGetImage(imageCache, 0));
+}
+
 
 function ExecuteFrame(dt: number, state: State) {
   if (!state.hasCompiledOnce) {
@@ -607,154 +775,8 @@ function ExecuteFrame(dt: number, state: State) {
 
   if (legitFrame) {
     try {
-      ImageCacheStartFrame(
-        state.gpu.gl,
-        state.imageCache,
-      )
-      //This image will be copied into the swapchain
-      ImageCacheProcessRequest(
-        state.gpu.gl,
-        state.imageCache,
-        {id : 0, pixel_format : 'rgba8', size : {x: gpu.canvas.width, y: gpu.canvas.height}, type : 'CachedImageRequest'},
-        console.error)
-    
-      for(const request of legitFrame.context_requests){
-        if(request.type === 'CachedImageRequest'){
-          ImageCacheProcessRequest(
-            state.gpu.gl,
-            state.imageCache,
-            request,
-            console.error
-          )
-        }
-        if(request.type == 'FloatRequest'){
-          state.processedRequests.push({
-            name : request.name,
-            type : 'float',
-            value : state.imControlsCallbacks.floatSlider(request.name, request.def_val, request.min_val, request.max_val)
-          });
-        }
-        if(request.type == 'IntRequest'){
-          state.processedRequests.push({
-            name : request.name,
-            type : 'int',
-            value : state.imControlsCallbacks.intSlider(request.name, request.def_val, request.min_val, request.max_val)
-          });
-        }
-        if(request.type == 'TextRequest'){
-          state.imControlsCallbacks.text(request.text)
-        }
-        if(request.type == 'BoolRequest'){
-          state.processedRequests.push({
-            name : request.name,
-            type : 'int',
-            value : 1 //TODO: actually make a checkbox
-          });
-        }
-        if(request.type == 'LoadedImageRequest'){
-          //TODO: figure this out
-        }
-      }
-      for (const invocation of legitFrame.shader_invocations) {
-        const pass = state.framegraph.passes[invocation.shader_name]
-        gl.useProgram(pass.program)
-
-        for (
-          let uniformIndex = 0;
-          uniformIndex < invocation.uniforms.length;
-          uniformIndex++
-        ) {
-          const uniform = invocation.uniforms[uniformIndex]
-          if (!uniform) {
-            continue
-          }
-
-          switch (uniform.type) {
-            case 'float': {
-              gl.uniform1f(pass.uniforms[uniformIndex], uniform.value)
-              break
-            }
-            case 'vec2': {
-              gl.uniform2f(pass.uniforms[uniformIndex], uniform.value.x, uniform.value.y)
-              break
-            }
-            case 'vec3': {
-              gl.uniform3f(pass.uniforms[uniformIndex], uniform.value.x, uniform.value.y, uniform.value.z)
-              break
-            }
-            case 'vec4': {
-              gl.uniform4f(pass.uniforms[uniformIndex], uniform.value.x, uniform.value.y, uniform.value.z, uniform.value.w)
-              break
-            }
-            case "int": {
-              gl.uniform1i(pass.uniforms[uniformIndex], uniform.value)
-              break
-            }
-            case 'ivec2': {
-              gl.uniform2i(pass.uniforms[uniformIndex], uniform.value.x, uniform.value.y)
-              break
-            }
-            case 'ivec3': {
-              gl.uniform3i(pass.uniforms[uniformIndex], uniform.value.x, uniform.value.y, uniform.value.z)
-              break
-            }
-            case 'ivec4': {
-              gl.uniform4i(pass.uniforms[uniformIndex], uniform.value.x, uniform.value.y, uniform.value.z, uniform.value.w)
-              break
-            }
-          }
-        }
-
-        for (
-          let samplerIndex = 0;
-          samplerIndex < invocation.image_sampler_bindings.length;
-          samplerIndex++
-        ) {
-          const sampler = invocation.image_sampler_bindings[samplerIndex]
-          const handle = ImageCacheGetImage(state.imageCache, sampler.id)
-          if (!handle) {
-            console.error("missing image from image cache %s", sampler)
-          }
-
-          gl.activeTexture(gl.TEXTURE0 + samplerIndex)
-          gl.bindTexture(gl.TEXTURE_2D, handle)
-          gl.uniform1i(pass.samplers[samplerIndex], samplerIndex)
-          gl.bindTexture(gl.TEXTURE_2D, handle)
-        }
-
-        // special case for swapchain image
-        {
-          // TODO: bind more than one output
-          gl.bindFramebuffer(gl.FRAMEBUFFER, pass.fbo)
-          
-          if (invocation.color_attachments.length != pass.fboAttachmentIds.length) {
-            console.error("Mismatch in invocation vs pass description attachment count")
-          }
-          for (
-            let attachmentIndex = 0;
-            attachmentIndex < invocation.color_attachments.length;
-            attachmentIndex++
-          ) {
-            const attachment = invocation.color_attachments[attachmentIndex]
-            const target = ImageCacheGetImage(state.imageCache, attachment.id)
-            gl.framebufferTexture2D(
-              gl.FRAMEBUFFER,
-              pass.fboAttachmentIds[attachmentIndex],
-              gl.TEXTURE_2D,
-              target,
-              0
-            )
-          }
-          // TODO: handle framebuffer completeness
-          gl.drawBuffers(pass.fboAttachmentIds)
-        }
-
-        gl.viewport(0, 0, gpu.canvas.width, gpu.canvas.height)
-        gpu.fullScreenRenderer()
-        gl.bindFramebuffer(gl.FRAMEBUFFER, null)
-      }
-      CopyTexToSwapchain(state, ImageCacheGetImage(state.imageCache, 0));
-      
+      state.processedRequests = ProcessScriptRequests(state.imControlsCallbacks, state.imageCache, {x: gpu.canvas.width, y: gpu.canvas.height}, gl, legitFrame.context_requests);
+      RunScriptInvocations(state.imageCache, state.gpu, state.framegraph.passes, legitFrame.shader_invocations)
     } catch (e) {
       // can console.log/console.error this, but it'll stuck in a busy loop until error resolves
     }
